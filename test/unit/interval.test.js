500070ab1cb9d8c5ecf914c6a4c7b2e5fb2cb84e
escobar2018 <antoine.escobar@ensiie.fr>
2019-09-26 17:24:11 +0200

    correction test1 function include

--- test/unit/interval.test.js
+++ test/unit/interval.test.js
@@ -3,23 +3,23 @@ const Interval = require('../../src/interval');
 test("Common interval", () => {
     const interval1 = new Interval(2,10);
     const interval2 = new Interval(8,15);
-    expect(interval1.overlaps(interval2).toBeTruthy);
+    expect(interval1.overlaps(interval2)).toBeTruthy;
 });
 
 test("No commom interval", () => {
     const interval1 = new Interval(2,10);
     const interval2 = new Interval(12,15);
-    expect(interval1.overlaps(interval2).toBeFalsy);
+    expect(interval1.overlaps(interval2)).toBeFalsy;
 });
 
 test("Interval is include", () => {
     const interval1 = new Interval(2,10);
     const interval2 = new Interval(4,9);
-    expect(interval1.includes(interval2).toBeTruthy);
+    expect(interval1.includes(interval2)).toBeTruthy;
 });
 
 test("Interval is NOT include", () => {
     const interval1 = new Interval(2,10);
     const interval2 = new Interval(6,15);
-    expect(interval1.includes(interval2).toBeFalsy);
+    expect(interval1.includes(interval2)).toBeFalsy;
 });
