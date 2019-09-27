14eac46330cff40067ab9e6060c23731ce5da2cb
escobar2018 <antoine.escobar@ensiie.fr>
2019-09-27 17:12:36 +0200

    fonction getTotalCount() OK

---
+++ src/book.reposiroy.test.js
@@ -0,0 +1,28 @@
+const BookRepository = require('./book.repository');
+
+describe('Book repository Save', function () {
+
+    test('Save a book', () => {
+
+        const dbMock = {
+            get : jest.fn().mockReturnThis(),
+            push : jest.fn().mockReturnThis(),
+            write : jest.fn().mockReturnThis()
+        };
+        const repository = new BookRepository(dbMock);
+        repository.save({id: 1, name: "Unit test"});
+
+        expect(dbMock.write.mock.calls.length).toBe(1);
+    });
+});
+
+test("Count element in db => equal to 1", () => {
+
+    const dbMock = {
+        get: jest.fn().mockReturnThis(),
+        size: jest.fn().mockReturnThis(),
+        value: jest.fn().mockReturnValue(1)
+    };
+    const repository = new BookRepository(dbMock);
+    expect(repository.getTotalCount()).toBe(1);
+});
---
+++ src/test-script.js
@@ -0,0 +1,14 @@
+const BookRepository = require('./book.repository');
+const db = require('./db')
+
+const repository = new BookRepository(db);
+
+
+repository.save({
+    'id' : 1,
+    "name" :"test",
+    'price' :6.1,
+    "added_at" : '2019-01-01'
+});
+
+console.log(repository.getTotalCount());
