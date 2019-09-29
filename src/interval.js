e4d1c3fd57b6f4d44902b3799b24065b51b3c162
escobar2018 <antoine.escobar@ensiie.fr>
2019-09-29 17:05:18 +0200

    intervals Intersection OK | Exclusion en-cours

--- src/interval.js
+++ src/interval.js
@@ -45,7 +45,13 @@ class Interval {
      * @returns {boolean}
      */
     includes(interval) {
-        return this.end > interval.end && this.start < interval.start;
+        let res = Boolean;
+        if(this.start <= interval.start && this.end >= interval.end){
+            res = true;
+        } else {
+            res = false;
+        }
+        return res;
     };
 
     /**
@@ -66,22 +72,32 @@ class Interval {
      */
     union(interval) {
         let tab_interval = [];
+        let interFusion = new Interval();
         // 2 intervals sont dissociés => 
-        if(this.end < interval.start || this.start > interval.end) {
+        if(this.end <= interval.start || this.start >= interval.end) {
             tab_interval.push(this);
             tab_interval.push(interval);
         // Exemple 1 (dans les 2 sens)
-        } else if(this.end > interval.start) {
-            let interFusion = new Interval();
+        } 
+        else if(this.end > interval.start) {
             interFusion.start = this.start;
             interFusion.end = interval.end;
             tab_interval.push(interFusion);
-        } else if(this.start > interval.end) {
-            let interFusion = new Interval();
+        } 
+        else if(interval.end > this.start) {
             interFusion.start = interval.start;
             interFusion.end = this.end;
             tab_interval.push(interFusion);
         }
+        else if(this.start <= interval.start && this.end >= interval.end) {
+            interFusion.start = this.start;
+            interFusion.end = this.end;
+        }
+        else if(interval.start <= this.start && interval.end >= this.end) {
+            interFusion.start = interval.start;
+            interFusion.end = interval.end;
+        }
+
         return tab_interval;
     };
 
@@ -98,12 +114,31 @@ class Interval {
      *      interval2 =                                                      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
      *      interval1.intersection(interval2) => <tableau vide>
      *
-     * @param {Interval} interval
+     * @param {Interval} interval 
      * @returns {Interval|null}
      */
     intersection(interval) {
         let tab_intersec = [];
-        
+        let point = 0;
+        // cas NO intersection
+        if( (this.end-interval.start) < 0 || (interval.end-this.start) < 0 ) {
+            return tab_intersec;
+        }
+        // cas où les 2 intervals ont un point en commun
+        else if( (this.end-interval.start) === 0 ) {
+            return point = interval.start;
+        }
+        else if( (this.start-interval.end) === 0 ) {
+            return point = this.start;
+        }
+        // cas interection : THIS devant Interval
+        else if( (this.end-interval.start) > 0 ) {
+            return point = this.end-interval.start;
+        } 
+        // cas intersection : Interval devant THIS
+        else if( (interval.start-this.end) > 0 ) { 
+            return point = interval.end-this.start;
+        }
     };
 
     /**
@@ -123,8 +158,44 @@ class Interval {
      * @returns {Interval[]}
      */
     exclusion(interval) {
-
+        let tab_exclu = [];
+        const exclu1 = new Interval();
+        const exclu2 = new Interval();
+        if( (interval.start-this.end) >= 0 || (this.start-interval.end) >= 0 ) {
+            tab_exclu.push(this);
+            tab_exclu.push(interval);
+            console.log(this); console.log(interval);
+            console.log(tab_exclu);
+            return tab_exclu;
+        }
+        else if( interval.start > this.end && interval.end > this.end && this.start < interval.start) {
+            exclu1.start = this.start;
+            exclu1.end = interval.start;
+            exclu2.start = this.end;
+            exclu2.end = interval.end;
+            tab_exclu.push(exclu1);
+            tab_exclu.push(exclu2);
+            console.log(this); console.log(interval);
+            console.log(tab_exclu);
+            return tab_exclu;
+        }
+        else if( this.start > interval.start && this.start < interval.end && this.end > interval.end) {
+            exclu1.start = interval.start;
+            exclu1.end = this.start;
+            exclu2.start = interval.end;
+            exclu2.end = this.end;
+            tab_exclu.push(exclu1);
+            tab_exclu.push(exclu2);
+            console.log(this); console.log(interval);
+            console.log(tab_exclu);
+            return tab_exclu;
+        }
     };
 }
+
+const interval1 = new Interval(2,10);
+const interval2 = new Interval(6,15);
+
+console.log(interval1.exclusion(interval2));
 
 module.exports = Interval;
